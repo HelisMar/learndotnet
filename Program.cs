7609410edde4a2441a6a73b69ef1e805ec61abd4
HelisMar <helismartoja@gmail.com>
2021-09-24 19:57:11 +0300

    if proov

--- Program.cs
+++ Program.cs
@@ -6,9 +6,23 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            int number;
-            number=1;
-            Console.WriteLine(number);
+            //int number;
+            //number=1;
+           
+
+    
+            // bool answer = true || true;
+
+            // bool answer = 10 < 1 || 2 >1 && 3 == 1;
+
+            int num = 10;
+            if(num<12){ 
+
+                Console.WriteLine("väiksem");
+            }
+
+             
+
         }
     }
 }
