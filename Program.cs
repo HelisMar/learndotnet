88db7e0f46026e8a89267eceefb6568f0645486f
HelisMar <helismartoja@gmail.com>
2021-09-28 18:15:33 +0300

    DateTime proov kodutööd teha.

--- Program.cs
+++ Program.cs
@@ -6,44 +6,14 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            try 
-            {
-             Console.WriteLine("Sisesta esimene number:");
-             int num1_= int.Parse(Console.ReadLine());
-             Console.WriteLine("Sisesta teine number:");
-             int num2 = int.Parse(Console.ReadLine());
-             Console.WriteLine("Siesta operatsioon (liida/lahuta / korruta / jaga):");
-             string operation = Console.ReadLine();
-             int anwser;
-             switch(operation) 
-            {
-                case "liida":
-                   anwser = num1_ + num2;
-                   break;
-                case "lahuta":
-                   anwser = num1_ - num2;
-                 break;
-                case "korruta":
-                  anwser = num1_ * num2;
-                  break;
-                case "jaga":
-                 anwser = num1_ / num2;
-                 break;
-                 default:
-                        throw(new Exception());
-
-                
-                 }
-
-          
-                Console.WriteLine("vastus on: " + anwser);
-                 
-                
-            }
-           catch {
-               Console.WriteLine("vale sisend!!");
+           DateTime date = new DateTime();
+           Console.WriteLine(date.ToString());
+        
+           
+            
            }
+            
 
         }
     }
-}
+
