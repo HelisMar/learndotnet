fb9a2046ddd5f1b946ed7e4875842da479dd5d8b
HelisMar <helismartoja@gmail.com>
2021-09-29 19:09:46 +0300

    Random numbri genereerimine

--- Program.cs
+++ Program.cs
@@ -6,9 +6,10 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-           DateTime date = new DateTime();
-           Console.WriteLine(date.ToString());
-        
+           Random random = new Random (); //teise randomi asemel võib ükskõik mis nime panna
+           int num = random.Next(50);
+           Console.WriteLine(num);
+
            
             
            }
