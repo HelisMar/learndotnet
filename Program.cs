c0173da58652657835bc97d96004143de66165c2
HelisMar <helismartoja@gmail.com>
2021-09-30 19:47:25 +0300

    Bob ja Alice kodutöö

--- Program.cs
+++ Program.cs
@@ -6,10 +6,20 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-
            Console.WriteLine("Mis on sinu nimi?");
            string name = Console.ReadLine();
-           Console.WriteLine("Tere " + name + "!");
+           
+         if(name == "Alice")
+         { Console.WriteLine("Tere " + name + "!");
+
+         } else if (name == "Bob")  {
+             Console.WriteLine("Tere " + name + "!");
+         }
+           else { Console.WriteLine ("Tere!") ;
+           
+           }
+         
+            
         }
 
         }
