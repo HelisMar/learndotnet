3ad5a455014d67b067fc5a5b73ba9cc206679a14
HelisMar <helismartoja@gmail.com>
2021-09-30 19:27:54 +0300

    Mis on sinu nimi kodutöö

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-           Console.WriteLine ("Hello world!");
-           }
-            
+
+           Console.WriteLine("Mis on sinu nimi?");
+           string name = Console.ReadLine();
+           Console.WriteLine("Tere " + name + "!");
+        }
 
         }
     }
