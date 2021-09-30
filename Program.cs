eb5c4c1b59d1b9d370dc5904dfb297e0ca0dff04
HelisMar <helismartoja@gmail.com>
2021-09-30 19:07:09 +0300

    Kodutöö hello world

--- Program.cs
+++ Program.cs
@@ -6,12 +6,7 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-           Random random = new Random (); //teise randomi asemel võib ükskõik mis nime panna
-           int num = random.Next(50);
-           Console.WriteLine(num);
-
-           
-            
+           Console.WriteLine ("Hello world!");
            }
             
 
