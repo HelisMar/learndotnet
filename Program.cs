e049bb4cb02d3c0cc1afd1f4cabc7c16971f767e
HelisMar <helismartoja@gmail.com>
2021-10-01 19:24:33 +0300

    Sorteerimine 01.10.21

--- Program.cs
+++ Program.cs
@@ -8,12 +8,29 @@ namespace learndotnet
         static void Main(string[] args)
         {
          
-         List<int> numbers = new List<int>();
-         numbers.Add(10);
-         Console.WriteLine(numbers[0]);
-         bool contains = numbers.Contains(2);
-         Console.WriteLine(contains);
-            
+         int[] array = {5,2,6,1,4,3,4}; 
+         int[] result = InsertSort(array);
+         foreach(int num in result){
+                Console.WriteLine(num);
+            }
+        }
+        static int[] InsertSort(int[]array){
+            for(int i = 0; i< array.Length; i++){
+                int min = array[i];
+                int minPos = i;
+                for(int j = i; j<array.Length;j++){
+                    if(array[j]<min) {
+                        min = array[j];
+                        minPos = j;
+                    }
+                }
+                int temp=array[i];
+                array[i] = min;
+                array[minPos] = temp;
+
+            }
+            return array;
+
         }
 
         }
