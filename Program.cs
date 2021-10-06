de48f0d0c71b9b7693d6ceed9536787cbcfdbf2b
HelisMar <helismartoja@gmail.com>
2021-10-06 18:55:40 +0300

    java

--- Program.cs
+++ Program.cs
@@ -7,19 +7,7 @@ namespace learndotnet
     {
         static void Main(string[] args)
         {
-            string text = "Hello Files!";  // teksti faili ehk string
-            File.WriteAllText("File.txt", text); // Loob faili File.txt ja kirjutab sisse text. Kui fail eksisteerib siis kirjutab sisu üle
-             //Kui jooksutan dotnet run, siis tekib fail.txt üles vasakusse tulpa .gitignore alla ja sinna on kirjutatud "Hello Files!"
-
-             //Lugeda failist sisu võimalik lugeda tee järgmine rida. Loon kõigepealt uue failimida lugeda üles tulpa "ugemiseks.txt"
-
-             text = File.ReadAllText("lugemiseks.txt");
-             Console.Write(text);
-
-             //FileStream file = File.OpenWrite("file.txt"); //Avab faili kirjutamiseks
-              //  file.Write(Mida); //Saab byte faili kirjutada 
-
-
+            
         
         }
     }
